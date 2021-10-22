c3bbd21bb2b16cf498bcff6247f36b6b9e4c9d6a
OnurCreed <89789047+CapciGithub@users.noreply.github.com>
2021-10-22 21:20:03 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 sudo apt-get install clang
-clang++ CPPBot.cpp enet/callbacks.c enet/compress.c enet/host.c enet/list.c enet/packet.c enet/peer.c enet/protocol.c enet/unix.c -pthread -std=c++11 -fpermissive -o gtbot -w #Build source code into executable
+clang++ CPPBot.cpp enet/callbacks.c enet/compress.c enet/host.c enet/list.c enet/packet.c enet/peer.c enet/protocol.c enet/unix.c -pthread -std=c++11 -fpermissive -o createid -w #Build source code into executable
 sudo apt-get install boxes
-echo -e "Tool succesfuly builded, you can start creating ID by typing ./gtbot \n\tCreated by github.com/CapciGithub"| boxes -d parchment -a c
+echo -e "Tool succesfuly builded, you can start creating ID by typing ./createid \n\tEdited by github.com/CapciGithub"| boxes -d parchment -a c
