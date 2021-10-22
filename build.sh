6343101f5038767283602092ae4d643916292d1a
OnurCreed <89789047+CapciGithub@users.noreply.github.com>
2021-10-22 21:12:50 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+sudo apt-get install clang
 clang++ CPPBot.cpp enet/callbacks.c enet/compress.c enet/host.c enet/list.c enet/packet.c enet/peer.c enet/protocol.c enet/unix.c -pthread -std=c++11 -fpermissive -o gtbot -w #Build source code into executable
 sudo apt-get install boxes
 echo -e "Tool succesfuly builded, you can start creating ID by typing ./gtbot \n\tCreated by github.com/CapciGithub"| boxes -d parchment -a c
