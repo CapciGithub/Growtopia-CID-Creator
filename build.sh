77bdc062591eb944df1d12e12b3be2a009466dd0
OnurCreed <89789047+CapciGithub@users.noreply.github.com>
2021-10-22 20:26:53 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
-# If you already install g++ or build-essential, use this
-clang++ CPPBot.cpp enet/callbacks.c enet/compress.c enet/host.c enet/list.c enet/packet.c enet/peer.c enet/protocol.c enet/unix.c -Wall -std=c++11 -fpermissive -o gtbot -w #Build source code into executable
-echo "Tool Successfully Builded,"
-echo "You Can Start Creating ID By Typing ./gtbot"
+clang++ CPPBot.cpp enet/callbacks.c enet/compress.c enet/host.c enet/list.c enet/packet.c enet/peer.c enet/protocol.c enet/unix.c -pthread -std=c++11 -fpermissive -o gtbot -w #Build source code into executable
+sudo apt-get install boxes
+echo -e "Tool succesfuly builded, you can start creating ID by typing ./gtbot \n\tCreated by github.com/CapciGithub"| boxes -d parchment -a c
